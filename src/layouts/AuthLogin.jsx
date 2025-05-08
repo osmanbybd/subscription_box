803e399e261bf9928de61bf58bf013baa5866d79
osmanbybd <mreosmangoni@gmail.com>
2025-05-08 20:21:46 +0600

    again code submit

--- src/layouts/AuthLogin.jsx
+++ src/layouts/AuthLogin.jsx
@@ -14,7 +14,7 @@ const AuthLogin = () => {
     
     return (
         <div>
-            <div className='border border-gray-200 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 shadow-lg mb-4'>
+            <div className='border p-2 border-gray-200 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 shadow-lg mb-4'>
             <Navbar></Navbar>
             </div>
             <div className='bg-gray-200 min-h-[92vh] py-5 lg:py-9 px-4 lg:px6  '>
